273d96896714af2a8b2783b0cb34f60557c1e4a0
John Munsch <john.munsch@gmail.com>
2019-09-02 11:48:09 -0500

    Added comments and slightly tweaked the code.

--- index.js
+++ index.js
@@ -7,8 +7,33 @@ const path = require('path');
 
 const app = express();
 
-// Certificate
-const domain = 'node-https.ml';
+const domain = 'node-https.ga';
+
+// The express.json() line isn't necessary at the moment because we aren't POSTing any
+// data to this server, but it will come in handy later if we start using this to build
+// an API.
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Automatic redirection from HTTP to HTTPS.
+app.use((req, res, next) => {
+  if (req.secure) {
+    next();
+  } else {
+    res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+});
+
+// Just a stupid example URL. Obviously, your real server would go here.
+app.get('/', (req, res) => res.send('It Works!'));
+
+// Create servers.
+http.createServer(app).listen(80, () => {
+  console.log(`Listening on http://${domain}`);
+});
+
+// Create credentials using Let's Encrypt certificate.
 const credentials = {
   key: fs.readFileSync(`/etc/letsencrypt/live/${domain}/privkey.pem`, 'utf8'),
   cert: fs.readFileSync(
@@ -17,26 +42,7 @@ const credentials = {
   ),
   ca: fs.readFileSync(`/etc/letsencrypt/live/${domain}/chain.pem`, 'utf8')
 };
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public'), { dotfiles: 'allow' }));
-
-app.use((req, res, next) => {
-  if (req.secure) {
-    next();
-  } else {
-    res.redirect(`https://${req.headers.host}${req.url}`);
-  }
-});
-app.get('/', (req, res) => res.send('It Works!'));
-
-// Create servers.
-let httpServer = http.createServer(app);
-httpServer.listen(80, () => {
-  console.log(`Listening on http://${domain}`);
-});
-
+// And use them to instantiate the HTTPS server.
 https.createServer(credentials, app).listen(443, () => {
   console.log(`and on https://${domain}`);
 });
