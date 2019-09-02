db71cf62657bb1372362b1265d6aa0a55d9f8c28
John Munsch <john.munsch@gmail.com>
2019-09-02 10:39:43 -0500

    Disabling the HTTPS server for initial checkout.

--- index.js
+++ index.js
@@ -9,15 +9,14 @@ const app = express();
 
 // Certificate
 const domain = 'node-https.ml';
-
-const credentials = {
-  key: fs.readFileSync(`/etc/letsencrypt/live/${domain}/privkey.pem`, 'utf8'),
-  cert: fs.readFileSync(
-    `/etc/letsencrypt/live/${domain}/fullchain.pem`,
-    'utf8'
-  ),
-  ca: fs.readFileSync(`/etc/letsencrypt/live/${domain}/chain.pem`, 'utf8')
-};
+// const credentials = {
+//   key: fs.readFileSync(`/etc/letsencrypt/live/${domain}/privkey.pem`, 'utf8'),
+//   cert: fs.readFileSync(
+//     `/etc/letsencrypt/live/${domain}/fullchain.pem`,
+//     'utf8'
+//   ),
+//   ca: fs.readFileSync(`/etc/letsencrypt/live/${domain}/chain.pem`, 'utf8')
+// };
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -30,6 +29,6 @@ http.createServer(app).listen(80, () => {
   console.log('HTTP Server running on port 80');
 });
 
-https.createServer(credentials, app).listen(443, () => {
-  console.log('HTTPS Server running on port 443');
-});
+// https.createServer(credentials, app).listen(443, () => {
+//   console.log('HTTPS Server running on port 443');
+// });
